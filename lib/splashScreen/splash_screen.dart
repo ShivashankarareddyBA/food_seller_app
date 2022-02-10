1e05f2efbdd87735f6754e21ec1039969fc24983
ShivashankarareddyBA <ssr2tech@gmail.com>
2022-02-10 13:39:31 +0530

    Food-seller-app

---
+++ lib/splashScreen/splash_screen.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+//import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:food_seller_app/authentication/auth_screen.dart';
+import 'package:food_seller_app/global/global.dart';
+import 'package:food_seller_app/mainScreens/home_screen.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  startTimer() {
+    Timer(const Duration(seconds: 1), () async {
+      // if seller is already logged in
+      if (firebaseAuth.currentUser != null) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => const HomeScreen()));
+      } else {
+        // if seller is already not logged in
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => const AuthScreen()));
+      }
+    });
+  }
+
+  //call function
+  @override
+  void initState() {
+    super.initState();
+
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Image.asset("images/splash.jpg"),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Seller App",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 40,
+                    fontFamily: "Signatra",
+                    letterSpacing: 3,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/loading_dialog.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:food_seller_app/widgets/progress_bar.dart';
+
+class LoadingDialog extends StatelessWidget {
+  final String message;
+
+  const LoadingDialog({required this.message}); // loading constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        key: key,
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            circularProgress(),
+            const SizedBox(height: 10),
+            Text(message + "Please Wait.."),
+          ],
+        ));
+  }
+}
